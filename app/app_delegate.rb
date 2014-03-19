831a33eb8655c6a70e76279f11232b47e8f50843
Devon Blandin <dblandin@gmail.com>
2014-03-19 14:52:19 -0500

    Add empty app

---
+++ app/app_delegate.rb
@@ -0,0 +1,5 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions: options)
+    true
+  end
+end
---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+describe 'motion-lens' do
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it 'has one window' do
+    @app.windows.size.should == 1
+  end
+end
